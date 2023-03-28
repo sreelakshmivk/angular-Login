00555d2b757d4331e0cf66aa91b20dedc7cb25ef
sreelakshmivk <sreelakshmivk0@gmail.com>
2023-03-28 18:56:16 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 @NgModule({
   imports: [BrowserModule],
-  declarations: [],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
