b641014cbb832dcb8e82cc52a020d2592ee73fe0
sreelakshmivk <sreelakshmivk0@gmail.com>
2023-03-28 22:04:32 +0530

    6 files modified, 3 added , Login Component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignUpComponent } from './auth/sign-up/sign-up.component';
 
 @NgModule({
   imports: [BrowserModule, AppRoutingModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, SignUpComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
